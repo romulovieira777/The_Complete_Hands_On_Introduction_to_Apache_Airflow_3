95ae8007b9444e509ab003e9ec1404d7a5c4fe59
RomuloVieira777 <romulo.vieira777@gmail.com>
2026-06-05 15:42:31 -0300

    Coding Your First Data Pipeline with Airflow

--- Section_04_Coding_Your_First_Data_Pipeline_With_Airflow/user_processing.py
+++ Section_04_Coding_Your_First_Data_Pipeline_With_Airflow/user_processing.py
@@ -1,24 +1,12 @@
 from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-
-def _extract_user(ti):
-    #fake_user = ti.xcom_pull(task_ids="is_api_available")
-    import requests
-    response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json")
-    fake_user = response.json()
-    return {
-        "id": fake_user["id"],
-        "firstname": fake_user["personalInfo"]["firstName"],
-        "lastname": fake_user["personalInfo"]["lastName"],
-        "email": fake_user["personalInfo"]["email"],
-    }
 
 @dag
 def user_processing():
-    
+
     create_table = SQLExecuteQueryOperator(
         task_id="create_table",
         conn_id="postgres",
@@ -46,11 +34,42 @@ def user_processing():
             fake_user = None
         return PokeReturnValue(is_done=condition, xcom_value=fake_user)
 
-    extract_user = PythonOperator(
-        task_id="extract_user",
-        python_callable=_extract_user
-    )
+    @task
+    def extract_user(fake_user):
+        return {
+            "id": fake_user["id"],
+            "firstname": fake_user["personalInfo"]["firstName"],
+            "lastname": fake_user["personalInfo"]["lastName"],
+            "email": fake_user["personalInfo"]["email"],
+        }
 
-    is_api_available()
-    
+    @task
+    def process_user(user_info):
+        import csv
+
+        user_info = {
+            "id": "123",
+            "firstname": "John",
+            "lastname": "Doe",
+            "email": "john.doe@example.com",
+        }
+
+        with open("/tmp/user_info.csv", "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=user_info.keys())
+            writer.writeheader()
+            writer.writerow(user_info)
+
+    @task
+    def store_user():
+        hook = PostgresHook(postgres_conn_id="postgres")
+        hook.copy_expert(
+            sql="COPY users FROM STDIN WITH CSV HEADER",
+            filename="/tmp/user_info.csv"
+        )
+
+    fake_user = is_api_available()
+    user_info = extract_user(fake_user)
+    process_user(user_info)
+    store_user()
+
 user_processing()
